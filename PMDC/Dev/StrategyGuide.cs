2aa4b95b449d5ca75f228ef68c91a1bf9f6cf2ac
EthanLac13 <83074801+EthanLac13@users.noreply.github.com>
2026-08-14 13:24:18 -0400

    Changed file formatitng

--- PMDC/Dev/StrategyGuide.cs
+++ PMDC/Dev/StrategyGuide.cs
@@ -1540,7 +1540,7 @@ namespace PMDC.Dev
 
                     if (fileContent.Length > 0)
                     { 
-                        string fileName = zoneName.Replace("\r\n", "").Replace("\r", "").Replace("\n", "").Replace("B{0}F", "").Replace("{0}F", "");
+                        string fileName = zoneName.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ").Replace("B{0}F", "").Replace("{0}F", "").TrimEnd();
                         WriteToWiki(fileName + "/Encounters", fileContent);
                     }
                 }
